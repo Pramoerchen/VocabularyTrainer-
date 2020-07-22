fed344d985ec567b40c18c141b5da33c3143f294
Pramoerchen <65039296+Pramoerchen@users.noreply.github.com>
2020-07-22 19:57:25 +0200

    first commit

---
+++ Vokabeltrainer/Vokabeltrainer/Vokabeltrainer/VokabelEingabe.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Vokabeltrainer
+{
+    /// <summary>
+    /// Interaktionslogik für Window1.xaml
+    /// </summary>
+    public partial class VokabelEingabe : Window
+    {
+        public VokabelEingabe()
+        {
+            InitializeComponent();
+        }
+
+        private void Zurück_Klick(object sender, RoutedEventArgs e)
+        {
+            MainWindow objMainWindow = new MainWindow(); // referenz auf das Fenster VokabelEingabe
+            this.Visibility = Visibility.Hidden; //lässt das menü verschwinden
+            objMainWindow.Show(); // wechselt zur ins "VokabelEingabe" fenster
+        }
+    }
+}
